46af259b5dbe1bc6c87412d234d33e7c401d562d
Faustino Martinez <famartin@student.42.us.org>
2018-07-22 16:30:11 -0700

    first

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: famartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/22 17:17:40 by famartin          #+#    #+#             */
+/*   Updated: 2018/03/23 10:52:25 by famartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define BUFF_SIZE 8
+# define MAX_FD 1024 + 1
+# define RET_VALUE(ret)	ret > 0 ? 1 : ret
+
+int		get_next_line(int const fd, char **line);
+
+#endif
